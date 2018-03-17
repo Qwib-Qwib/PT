a41623a8a9fedc400a14bb2069d3ee3cad653dd1
Sasha Markov <sasha.markov.mail@gmail.com>
2018-03-17 22:12:21 +0300

    1

--- Mindbox/query.sql
+++ Mindbox/query.sql
@@ -1,11 +1,10 @@
-select c.cutomerid
-      ,name
-from customers c inner join orders o on c.customerId = o.customer.Id 
+select c.customerId
+      ,c.name
+from customers c inner join orders o on c.customerId = o.customerId 
 where o.product_name in ('Milk', 'SourCream')
-group by c.customerid, c. name
+group by c.customerId, c.name
 having( sum(case when o.product_name = 'Milk' then 1 else 0 end) > 0
 	and sum(case when o.product_name = 'SourCream' 
-				  and datepart(y, o.purchasedate) = datepart(y, getdate()
-				  and datepart(m, o.purchasedate) = datepart(m, getdate())
-	    			then 1 else 0 end) = 0 
+				  and datepart(year, o.purchasedate) = datepart(year, getdate())
+				  and datepart(month, o.purchasedate) = datepart(month, getdate()) then 1 else 0 end) = 0 
 	  )
